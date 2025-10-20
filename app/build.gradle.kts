03c1e34e1a4e56189ae402f6f86b40f4d653185f
Jonathan Casagrande Filho <jcasagrandefilho@protonmail.com>
2025-10-20 15:33:40 +0100

    Adding splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,13 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.core.splashscreen)
+
+
+    implementation(libs.androidx.navigation.runtime.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
